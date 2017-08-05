41f54b9a9957f5112ed47596d137ed3dd11beb1d
shilpakolpe <shilpasrikp@gmail.com>
2017-08-05 12:06:30 +0530

    delete

--- day.java
+++
@@ -1,7 +0,0 @@
-class day
-{
-public static void main(String [] skp)
-{
-System.out.println("display day as monday");
-}
-}
--- newtest.java
+++
@@ -1,8 +0,0 @@
-class newtest
-{
-public static void main(String args[])
-{
-int a=1000;
-System.out.println("the value of a is :"+a);
-}
-}
